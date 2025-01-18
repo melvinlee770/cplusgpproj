5887fc393246517226e47228718792b1c7df9e3b
melvinlee770 <melvinlee0199@gmail.com>
2025-01-18 17:16:49 +0800

    make the void prepare vehicle settings and done for allocate energy

--- Proj2Auto.cpp
+++ Proj2Auto.cpp
@@ -2,23 +2,42 @@
 #include <string>
 #include "SatComRelay.h"
 
-int main() {
-    // Instantiate the SatComRelay object
-    SatComRelay satComRelay;
+void PrePareVehicle(const std::string &srcFileName, bool fileNeedsDecryption, bool randomizeStartPosition, int missionType);
+                    
 
+int main() {
+	
     // parameters for the function
-    std::string srcFilename = "/home/student/Downloads/GrpProj_Pkg/Scenes/Scenario06.dat";
+    std::string srcFileName = "/home/student/Downloads/GrpProj_Pkg/Scenes/Scenario06.dat";
     bool fileNeedsDecryption = true;           // Set to true if the file requires decryption
     bool randomizeStartPosition = true;         // Set as needed for your scenario
     int missionType = 1;                        // Example mission type; adjust based on your needs
 
-	VehData vehicleData = satComRelay.initializeVehicle(srcFilename, fileNeedsDecryption, randomizeStartPosition, missionType);
-	/*
-    std::cout << "Initial Energy: " << vehicleData.getInitialEnergy() << std::endl;
-    std::cout << "Current Energy: " << vehicleData.getCurrentEnergy() << std::endl;
-    std::cout << "Current Shield Energy: " << vehicleData.getCurrentShieldEnergy() << std::endl;
-  	*/
+	PrePareVehicle(srcFileName, fileNeedsDecryption, randomizeStartPosition, missionType);
 
     return 0;
 }
+
+void PrePareVehicle(const std::string &srcFileName, bool fileNeedsDecryption, bool randomizeStartPosition, int missionType) {
+	
+	SatComRelay satComRelay;
+
+	VehData vehicleData = satComRelay.initializeVehicle(
+		srcFileName, fileNeedsDecryption, randomizeStartPosition, missionType
+	);
+	vehicleData = satComRelay.allocateEnergyToShield(50000);
+	
+	std::cout << std::endl;
+	std::cout << "##VEHICLE STATUS##" << std::endl;
+    std::cout << "Initial Energy: " << vehicleData.getInitialEnergy() << std::endl;
+    std::cout << "Current Energy: " << vehicleData.getCurrentEnergy() << std::endl;
+    std::cout << "Current Shield Energy: " << vehicleData.getCurrentShieldEnergy() << std::endl;
+  	
+  	
+}
+
+
+
+
+
 
